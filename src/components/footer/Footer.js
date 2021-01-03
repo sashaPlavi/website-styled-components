ad6af13bea33beace6c4f8694be9745393e1478e
SashaPlavi <sasha.bingulac@gmail.com>
2021-01-03 18:19:13 +0100

    social footer;

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Button } from '../../globalStyles';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from 'react-icons/fa';
 
 import {
   FooterContainer,
@@ -13,6 +20,13 @@ import {
   FooterLinkTitle,
   FooterLinksItems,
   FooterLink,
+  SocialMedia,
+  SocialMediaWrapper,
+  SocialIcons,
+  SocialIcon,
+  SocialLogo,
+  SocialIconLink,
+  WebsiteRights,
 } from './Footer.style';
 
 const Footer = () => {
@@ -67,6 +81,37 @@ const Footer = () => {
             </FooterLinksItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrapper>
+            <SocialLogo to="/">
+              <SocialIcon />
+              Ultra
+            </SocialLogo>
+            <WebsiteRights>Ultra 2020</WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink
+                href={'https://www.youtube.com/watch?v=iP_HqoCuRI0'}
+                target="blank"
+                aria-label="Youtube"
+                rel="noopener noreferrer"
+              >
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrapper>
+        </SocialMedia>
       </FooterContainer>
     </div>
   );
